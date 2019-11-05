6e23c964d04af02e4bf38ca7b1e97b61326a8cdc
McLarenCollege <hello@mclarencollege.in>
2019-11-05 08:15:26 +0530

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,25 @@
+// Challenge 1
+// Write test cases for the given challenges
+
+// Challenge 2
+// Pandigital Numbers
+// A pandigital number contains all digits (0-9) at least once. Write a function
+// that takes an integer, returning true if the integer is pandigital, and false otherwise.
+//  Examples
+//  isPandigital(98140723568910) ➞ true
+
+// Challenge 3
+// Area of Overlapping Rectangles
+// Create a function that returns the area of the overlap between two rectangles.
+// The function will receive two rectangles, each with the coordinates of two of
+// its opposite angles.
+//
+// Examples
+// overlappingRectangles(
+//    [{ x: 2, y: 1 }, { x: 5, y: 5 }],
+//    [{ x: 3, y: 2 }, { x: 5, y: 7 }]
+//  ) ➞ 6
+
+main() {
+
+}
---
+++ test/Day_24_Morning_Challenge_test.dart
@@ -0,0 +1,8 @@
+import 'package:Day_24_Morning_Challenge/Day_24_Morning_Challenge.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
